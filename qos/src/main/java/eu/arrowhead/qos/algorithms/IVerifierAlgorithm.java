76bf8d4fb3b16a10ec54ba795467964e6ad017ac
FlavioRelvas <1140826@isep.ipp.pt>
2018-11-27 19:24:02 +0000

    Added missing headers to changed files

--- qos/src/main/java/eu/arrowhead/qos/algorithms/IVerifierAlgorithm.java
+++ qos/src/main/java/eu/arrowhead/qos/algorithms/IVerifierAlgorithm.java
@@ -11,12 +11,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * This work was partially supported by National Funds through FCT/MCTES (Portuguese Foundation
- * for Science and Technology), within the CISTER Research Unit (CEC/04234) and also by
- * Grant nr. 737459 Call H2020-ECSEL-2016-2-IA-two-stage 
- * ISEP/CISTER, Polytechnic Institute of Porto.
- * Luis Lino Ferreira (llf@isep.ipp.pt), Flávio Relvas (flaviofrelvas@gmail.com),
- * Michele Albano (mialb@isep.ipp.pt), Rafael Teles Da Rocha (rtdrh@isep.ipp.pt)
+ * This work was supported by National Funds through FCT (Portuguese
+ * Foundation for Science and Technology) and by the EU ECSEL JU
+ * funding, within Arrowhead project, ref. ARTEMIS/0001/2012,
+ * JU grant nr. 332987.
+ * ISEP, Polytechnic Institute of Porto.
  */
 package eu.arrowhead.qos.algorithms;
 
